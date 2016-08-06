c98e947c55da72f1def1fdfb68b405c2b09249c4
Sean Hoyt <seanhoyt963@gmail.com>
2016-08-06 02:13:01 -0700

    Update kernel config for source built

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,13 +48,15 @@ TARGET_USES_64_BIT_BINDER := true
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=16M@0-0xffffffff androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_IMAGE_NAME := zImage
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-BOARD_MKBOOTIMG_ARGS := --tags_offset 0x00000100
+TARGET_KERNEL_SOURCE := kernel/leeco/msm8996
+TARGET_KERNEL_CONFIG := cyanogenmod_x2_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-TARGET_USES_UNCOMPRESSED_KERNEL := false
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
