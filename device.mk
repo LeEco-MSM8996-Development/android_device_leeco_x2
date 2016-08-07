7276bbb31225fa882141d7773309dc5210f13d10
Sean Hoyt <seanhoyt963@gmail.com>
2016-08-07 05:38:17 -0700

    Move QMI configs to vendor

--- device.mk
+++ device.mk
@@ -256,12 +256,6 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc \
     init.qcom.bt.sh
 
-# QMI
-PRODUCT_PACKAGES += \
-    dsi_config.xml \
-    netmgr_config.xml \
-    qmi_config.xml \
-
 # Sensors
 PRODUCT_PACKAGES += \
     sensors.msm8996
