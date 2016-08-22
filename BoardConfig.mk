26c53ba854b884ba4aade1845adc6ee70c363a5a
Ncrypt3d <Ncrypt3d@users.noreply.github.com>
2016-08-22 11:39:58 -0500

    Add NTFS OTG support
    May be underlying issues with OTG...WIP. Starting by ruling out potential user hardware variances and format issues.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,6 +48,7 @@ BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_INCLUDE_NTFS_3G := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 
 # Encryption
